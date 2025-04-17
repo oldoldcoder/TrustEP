f592381951c9510027569d5bd8ed66e857309d72
AsyncKurisu <1750981157@qq.com>
2025-04-17 11:40:18 +0800

    更改了数据模型

--- evaluate/models.py
+++ evaluate/models.py
@@ -1,38 +1,33 @@
 from django.db import models
 
 
-class Identity(models.Model):
+class Local(models.Model):
+    tb_id = models.CharField(max_length=128, primary_key=True)
+    security_card_id = models.CharField(max_length=128)
     name = models.CharField(max_length=64)
-    security_card_id = models.CharField(max_length=128, primary_key=True)
-
-
-class Dynamic(models.Model):
-    security_card = models.ForeignKey(Identity, on_delete=models.CASCADE)
-    login_time = models.DateTimeField()
-    login_position = models.CharField(max_length=64)
-    device_ip = models.CharField(max_length=128)
-    cpu_id = models.CharField(max_length=256)
-    disk_id = models.CharField(max_length=256)
-    auth_type = models.IntegerField(default=0)
-    device_type = models.IntegerField(default=0)
-
-
-class Security(models.Model):
-    security_card = models.ForeignKey(Identity, on_delete=models.CASCADE)
+    device_ip = models.CharField(max_length=64)
+    device_site = models.CharField(max_length=64)
     cert_dn = models.CharField(max_length=256)
     cert_sn = models.CharField(max_length=256)
     soft_type = models.IntegerField(default=0)
     setup_type = models.IntegerField(default=0)
     os_type = models.IntegerField(default=0)
-    firewall_status = models.BooleanField()
-
-
-class Unauthorized(models.Model):
-    security_card = models.ForeignKey(Identity, on_delete=models.CASCADE)
-    times = models.IntegerField(default=0)
-    score = models.IntegerField(default=0)
-    position = models.CharField(max_length=128)
-    department = models.CharField(max_length=64)
+    oa_count = models.IntegerField(default=0)
+    oa_score = models.IntegerField(default=0)
     api_id = models.CharField(max_length=64)
     api_type = models.CharField(max_length=64)
+    department = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'tb_data_total'
+
+class TrustScore(models.Model):
+    api_id = models.CharField(max_length=128)
+    security_card_id = models.CharField(max_length=128)
     data_level = models.IntegerField(default=0)
+    result_code = models.IntegerField()
+    score = models.FloatField(default=0)
+    create_time = models.DateTimeField()
+
+    class Meta:
+        db_table = 'tb_historical_trust_scores'
